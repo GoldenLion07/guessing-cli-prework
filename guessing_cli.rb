eb1729e280662421d0ba34653c8b9fbb3f838fb0
Scott M <gmat191@gmail.com>
2018-10-17 22:44:09 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,4 @@
-def guessing_game
+def run_guessing_game
   puts "Guess a nubmer between 1 and 6."
   roll = 1+rand(6)
   user_input = gets.chomp.to_s 
@@ -13,5 +13,5 @@ def guessing_game
   end
 end
 
-guessing_game
+run_guessing_game
   
