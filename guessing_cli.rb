3444f7bdd62dfaaf55730af9b6dbd5ca91fab673
Scott M <gmat191@gmail.com>
2018-10-17 22:42:43 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,6 +6,7 @@ def guessing_game
     return "You guessed the correct number!"
     elsif user_input != roll 
     return "The computer guessed #{roll}!"
-    elsif user_input == exit 
+    elsif user_input == "exit" 
+    puts "Goodbye"
     
   
